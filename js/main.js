40be91b0a75244ad4f03aec9467ce6a064372360
mac72 <jmsegovi72@gmail.com>
2014-10-17 20:54:00 -0500

    estable 141017-20:53
    fin de preparaciones

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@
 $(document).on("ready", main);
 function main(){
 	var strPagina = "";
-
-  $("#bloqueDer").css("display", "none");  
+ 
+ $("#bloqueDer").css("display", "none");  
   $("#bloqueDer").fadeIn(500);
 
 	$("nav ul a").click(function(event){
        event.preventDefault();
-
-       	linkLocation = this.href
-       	if (linkLocation != window.location)
-       		$("#bloqueDer").fadeOut(500,redirectPage);
+	 	linkLocation = this.href
+	 	if (linkLocation != window.location) 
+	 		$("#bloqueDer").fadeOut(500,redirectPage);
     });
+	
 	strPagina = nombrePagina(location.pathname);
 	
 	switch (strPagina){
@@ -26,6 +26,7 @@ function main(){
 		case "eventos.html" :
 			$('#itemEventos').addClass('actual');
 			break;
+		default : $('#itemInicio').addClass('actual');
 	}
 
 } //fin de main
@@ -48,45 +49,5 @@ function nombrePagina(strpaht){
   return strpaht.substr(indice);
 }
 
-function irA () {
-	//alert(this.href);
-	 
-	//strPagina = nombrePagina(this.pathname);
-	//$(this).removeClass('actual');
-	//alert('');
-	//alert(pagina.href);
-	//alert('irA ' + prueba);
-	$(location).attr('href', this.href);
-  //$("#bloqueDer").fadeOut(3000);
-	/*switch (strPagina){
-		case "index.html":  
-		  //alert(strPagina); 
-			$('#itemInicio').addClass('actual');
-			break;
-		case "preparaciones.html" :
-			$('#itemPreparaciones').addClass('actual');
-			break;
-		case "eventos.html" :
-			$('#itemEventos').addClass('actual');
-			break;
-	}*/
 
-	//alert('aqui');
-	//$("#bloqueDer").fadeOut(1000)
-	//$("nav ul a").removeClass('actual')
-  
-	//$(this).addClass('actual');
-	//$("#bloqueDer").fadeIn(1000);
-	//$("body").fadeOut(3000);
-	//linkLocation = this.href;
-	//$(location).attr('href', linkLocation);
-	//$(location).attr('href',URL_PAGINA_WEB); 
-	//alert(linkLocation)
-	//redirectPage();
- //window.location.assign(this.href);
- //$(this).addClass('actual');
-	//return this;
-	//return false;
-	
-}//fin nde irA
 
